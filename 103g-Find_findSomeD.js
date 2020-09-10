4f0f0c97d342b0ec16b838b32b96311dba36d613
wildenali <wildenwildenaliali@gmail.com>
2020-09-10 19:32:08 +0700

    ini gagal

---
+++ 103g-Find_findSomeD.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var myDB = db.db("mydb");
+  myDB.collection("kastemers").find({}, { projection: { name: 1, address: 0 } }).toArray(function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
