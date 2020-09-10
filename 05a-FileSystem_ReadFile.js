03ab2932dcdcc40c2fdeb30b71a5f7a7c0818b36
wildenali <wildenwildenaliali@gmail.com>
2020-09-10 10:45:58 +0700

    File System, Cara Read File HTML

---
+++ 05a-FileSystem_ReadFile.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('05a-FileSystem_ReadFile.html', function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end();
+  })
+}).listen(8000);
