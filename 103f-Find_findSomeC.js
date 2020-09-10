87399a12da7fe8ab7f5a8cca00373fb636212333
wildenali <wildenwildenaliali@gmail.com>
2020-09-10 19:28:09 +0700

    Find, findSome yg tanpa _id nya

---
+++ 103f-Find_findSomeC.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var myDB = db.db("mydb");
+  myDB.collection("kastemers").find({}, { projection: { _id: 0 } }).toArray(function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
