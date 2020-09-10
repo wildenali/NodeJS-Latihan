cc9f9c97b4dc485ab735e10cc5234477e9e66b11
wildenali <wildenwildenaliali@gmail.com>
2020-09-10 21:33:47 +0700

    Sort, How to sort the document by name, sort ascending or descending

--- 105a-Sort_nameAsc.js
+++ 105a-Sort_nameAsc.js
@@ -4,7 +4,8 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
   var myDB = db.db("mydb");
-  var mySort = { name: 1 };
+  // var mySort = { name: 1 };
+  var mySort = { name: -1 };
   myDB.collection("kastemers").find().sort(mySort).toArray(function (err, result) {
     if (err) throw err;
     console.log(result);
