867e2b2323b0212ddf6f83fc76ad27d3ecd1381a
wildenali <wildenwildenaliali@gmail.com>
2020-09-11 13:55:13 +0700

    Global Objects

---
+++ 14a-GlobalObjects.js
@@ -0,0 +1,11 @@
+console.log(__filename);
+console.log(__dirname);
+
+function printHellow() {
+  console.log("halooow");
+}
+setTimeout(printHellow, 5000);
+// var t = setTimeout(printHellow, 5000);
+
+// Now clear the timer
+// clearTimeout(t)
